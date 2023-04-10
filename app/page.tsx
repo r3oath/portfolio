0613deabc7ae1ae856dfc7465dd7c64c1c5a561f
Tristan Strathearn <tristan@pixls.com.au>
2023-04-10 13:58:02 +1000

    fix: fix z-index on home page chaos element

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ const robotoMono = Roboto_Mono({subsets: ['latin']});
 
 const HeroSection = (): React.ReactElement => (
 	<div className="relative">
-		<ChaosOne className="hidden lg:block absolute bottom-0 -mb-[190px] blur-lg chaos"/>
+		<ChaosOne className="hidden lg:block absolute bottom-0 -mb-[190px] blur-lg chaos z-[-100]"/>
 		<div className="lg:absolute rounded-full mx-auto top-[50%] left-[50%] mt-16 lg:-mt-[280px] lg:-ml-[150px] w-[150px] lg:w-[300px] h-[150px] lg:h-[300px] bg-gradient-to-b from-gray-500 p-1 lg:p-2 z-20">
 			<Image
 				priority
