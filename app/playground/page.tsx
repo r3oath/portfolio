fb706ef1597c73c99a7e08c0e4efbe90d0acd432
Tristan Strathearn <tristan@pixls.com.au>
2023-04-10 14:18:24 +1000

    fix: fix playground page containers and chaos element

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -26,67 +26,71 @@ const items: PlaygroundItem[] = [
 });
 
 const PlaygroundPage = (): React.ReactElement => (
-	<div className="relative mx-auto px-4 pb-32 w-full max-w-5xl">
-		<ChaosTwo className="hidden lg:block absolute top-0 -mt-[270px] blur-lg chaos z-[-100]"/>
-		<h1 className="relative text-4xl lg:text-6xl font-extrabold mt-8 lg:mt-32 text-grey-200">{content.playground.heading}</h1>
-		<p className="relative text-lg lg:text-2xl text-gray-200 mt-8 text-justify markdown">
-			<Markdown components={{p: React.Fragment}}>{content.playground.subheading}</Markdown>
-		</p>
-		<p className="relative text-md lg:text-lg text-gray-400 mt-4 text-justify markdown">
-			<Markdown components={{p: React.Fragment}}>{content.playground.intro}</Markdown>
-		</p>
-		<p className="relative text-md lg:text-lg text-gray-400 mt-4 text-justify markdown">
-			<Markdown components={{p: React.Fragment}}>{content.playground.repo}</Markdown>
-		</p>
-		<div className="hidden lg:block relative border-b border-white/10 w-full mt-28">
-			<div className="absolute flex items-center justify-center left-[50%] top-[50%] -mt-4 -ml-6 w-12 h-8 bg-white/5 rounded backdrop-blur">
-				<CodeBracketIcon className="w-5 text-gray-200"/>
+	<div>
+		<div className="relative mx-auto px-4 pb-48 w-full max-w-5xl overflow-hidden">
+			<ChaosTwo className="hidden lg:block absolute top-0 left-[50%] w-[800px] -mt-[100px] -ml-[400px] blur-lg chaos z-[-100]"/>
+			<h1 className="relative text-4xl lg:text-6xl font-extrabold mt-8 lg:mt-32 text-grey-200">{content.playground.heading}</h1>
+			<p className="relative text-lg lg:text-2xl text-gray-200 mt-8 text-justify markdown">
+				<Markdown components={{p: React.Fragment}}>{content.playground.subheading}</Markdown>
+			</p>
+			<p className="relative text-md lg:text-lg text-gray-400 mt-4 text-justify markdown">
+				<Markdown components={{p: React.Fragment}}>{content.playground.intro}</Markdown>
+			</p>
+			<p className="relative text-md lg:text-lg text-gray-400 mt-4 text-justify markdown">
+				<Markdown components={{p: React.Fragment}}>{content.playground.repo}</Markdown>
+			</p>
+		</div>
+		<div className="relative mx-auto px-4 pb-32 w-full max-w-5xl -mt-48">
+			<div className="hidden lg:block relative border-b border-white/10 w-full mt-28">
+				<div className="absolute flex items-center justify-center left-[50%] top-[50%] -mt-4 -ml-6 w-12 h-8 bg-white/5 rounded backdrop-blur">
+					<CodeBracketIcon className="w-5 text-gray-200"/>
+				</div>
 			</div>
-		</div>
-		<div className="relative flex flex-wrap items-center justify-center md:gap-16 mt-8 xl:-ml-24 xl:-mr-24">
-			{content.playground.logos.map((logo) => (
-				<div key={logo.src} className="w-1/2 sm:w-1/3 p-4 md:p-0 md:flex-1">
-					<Image
-						className="w-full"
-						src={logo.src}
-						alt={logo.alt}
-						width={200}
-						height={200}
-					/>
-				</div>
-			))}
-		</div>
-		<p className="relative block lg:flex items-center mt-6 text-center gap-6">
-			<span className="border-b border-white/10 flex-1"/>
-			<span className="flex-shrink-0 text-white uppercase text-xs tracking-wider font-bold leading-6 markdown">
-				<Markdown components={{p: React.Fragment}}>{content.playground.tooling}</Markdown>
-			</span>
-			<span className="border-b border-white/10 flex-1"/>
-		</p>
-		<p className="block lg:hidden text-center max-w-xl mx-auto mt-16 bg-gray-900 border border-gray-600 p-6 rounded-lg">
-			<Markdown components={{p: React.Fragment}}>{content.playground.viewport}</Markdown>
-		</p>
-		<div className="hidden lg:block mt-24">
-			{items.map((item) => (
-				<div key={item.metadata.name}>
-					<h2 className="flex items-center">
-						<span className="uppercase text-xs font-bold text-gray-300 tracking-wide bg-gray-800 px-2 py-1 rounded-full">{item.metadata.granularity}</span>
-						<span className="ml-2 text-xl">{item.metadata.name}</span>
-					</h2>
-					<div className="flex items-center justify-between markdown">
-						<p className="text-gray-400">{item.metadata.description}</p>
-						<Link className="flex items-center text-gray-400" href={item.metadata.inspirationSource} target="_blank">
-							<ArrowTopRightOnSquareIcon className="w-4"/>
-							<span className="ml-2 text-sm">Reference</span>
-						</Link>
+			<div className="relative flex flex-wrap items-center justify-center md:gap-16 mt-8 xl:-ml-24 xl:-mr-24">
+				{content.playground.logos.map((logo) => (
+					<div key={logo.src} className="w-1/2 sm:w-1/3 p-4 md:p-0 md:flex-1">
+						<Image
+							className="w-full"
+							src={logo.src}
+							alt={logo.alt}
+							width={200}
+							height={200}
+						/>
 					</div>
-					<div className="border border-gray-800 bg-gray-900 shadow-lg rounded-lg mt-3 p-8">
-						<Suspense>
-							<item.component/>
-						</Suspense>
+				))}
+			</div>
+			<p className="relative block lg:flex items-center mt-6 text-center gap-6">
+				<span className="border-b border-white/10 flex-1"/>
+				<span className="flex-shrink-0 text-white uppercase text-xs tracking-wider font-bold leading-6 markdown">
+					<Markdown components={{p: React.Fragment}}>{content.playground.tooling}</Markdown>
+				</span>
+				<span className="border-b border-white/10 flex-1"/>
+			</p>
+			<p className="block lg:hidden text-center max-w-xl mx-auto mt-16 bg-gray-900 border border-gray-600 p-6 rounded-lg">
+				<Markdown components={{p: React.Fragment}}>{content.playground.viewport}</Markdown>
+			</p>
+			<div className="hidden lg:block mt-24">
+				{items.map((item) => (
+					<div key={item.metadata.name}>
+						<h2 className="flex items-center">
+							<span className="uppercase text-xs font-bold text-gray-300 tracking-wide bg-gray-800 px-2 py-1 rounded-full">{item.metadata.granularity}</span>
+							<span className="ml-2 text-xl">{item.metadata.name}</span>
+						</h2>
+						<div className="flex items-center justify-between markdown">
+							<p className="text-gray-400">{item.metadata.description}</p>
+							<Link className="flex items-center text-gray-400" href={item.metadata.inspirationSource} target="_blank">
+								<ArrowTopRightOnSquareIcon className="w-4"/>
+								<span className="ml-2 text-sm">Reference</span>
+							</Link>
+						</div>
+						<div className="border border-gray-800 bg-gray-900 shadow-lg rounded-lg mt-3 p-8">
+							<Suspense>
+								<item.component/>
+							</Suspense>
+						</div>
 					</div>
-				</div>
-			))}
+				))}
+			</div>
 		</div>
 	</div>
 );
